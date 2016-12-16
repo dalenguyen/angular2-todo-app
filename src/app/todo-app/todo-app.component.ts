3984b7dfaad9148afe7d16818ec8d4adc1ab5ac0
Dale Nguyen <dungnq@itbox4vn.com>
2016-12-16 06:23:14 +0000

    Add todo app compoment

---
+++ src/app/todo-app/todo-app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo'
+import { TodoService } from '../todo.service'
+
+@Component({
+  selector: 'todo-app',
+  templateUrl: './todo-app.component.html',
+  styleUrls: ['./todo-app.component.css'],
+  providers: [TodoService]
+})
+export class TodoAppComponent implements OnInit {
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit() {
+  }
+
+  newTodo: Todo = new Todo()
+
+  addTodo() {
+      this.todoService.addTodo(this.newTodo);
+      this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo){
+	this.todoService.toggleTodoComplete(todo)
+  }
+
+  removeTodo(todo) {
+      this.todoService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+      return this.todoService.getAllTodos();
+  }
+}
